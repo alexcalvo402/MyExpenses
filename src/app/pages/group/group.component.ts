8faf0bd6f16c90b3777d388d4181a8944ca66cec
Alex Calvo <alexcalvo402@gmail.com>
2023-11-05 21:33:33 +0100

    my group a partir de array

--- src/app/pages/group/group.component.ts
+++ src/app/pages/group/group.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./group.component.scss']
 })
 export class GroupComponent {
+  expenses:Array<expense> = [];
+  myTotal = 0;
+  total = 0;
 
+  ngOnInit(){
+    this.getExpenses();
+  }
+
+  getExpenses(){
+    this.expenses = [{
+      title: "Hotel",
+      buyer: "Yo",
+      price: 102,
+      date: new Date(),
+    },
+    {
+      title: "Telepizza",
+      buyer: "Ivan",
+      price: 24.95,
+      date: new Date(),
+    },
+    {
+      title: "Hotel",
+      buyer: "Yo",
+      price: 30,
+      date: new Date(),
+    }];
+
+    this.myTotal = 34.65;
+    this.total = 0;
+
+  }
+
+}
+
+interface expense{
+  title: string;
+  buyer: string;
+  price: number;
+  date: Date;
 }
