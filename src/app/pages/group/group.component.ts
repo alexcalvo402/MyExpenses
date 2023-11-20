198d12be8878af2f1b59e1d062e98bdeedb794ac
Alex Calvo <alexcalvo402@gmail.com>
2023-11-20 02:37:16 +0100

    expense

--- src/app/pages/group/group.component.ts
+++ src/app/pages/group/group.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { expense, expenseToAdd, groupExpense } from '../../interfaces/expense';
 import { ApiService } from 'src/app/services/api.service';
 import { NonNullableFormBuilder } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-group',
@@ -13,33 +14,48 @@ export class GroupComponent {
   expenses: Array<groupExpense> = [];
   myTotal = 0;
   total = 0;
-  id = "1";
+  group_id:number;
+  user_id: number;
 
-  constructor(protected apiService: ApiService, protected fb: NonNullableFormBuilder, protected activatedRoute: ActivatedRoute) { }
+  form = this.fb.group({
+    "title": ['',],
+    "amount": [0,],
+  });
+
+  constructor(protected apiService: ApiService, protected fb: NonNullableFormBuilder, protected activatedRoute:ActivatedRoute, protected userService:UserService) { 
+    this.user_id = this.userService.getUser().id;
+    this.group_id = parseInt(this.activatedRoute.snapshot.paramMap.get('id')!);
+
+  }
 
   ngOnInit() {
 
     this.activatedRoute.data.subscribe((data: any) => {
       this.expenses = data.expenses;
+
+      for(let i=0;i<this.expenses.length;i++){
+        let expense = this.expenses[i];
+
+        this.total += expense.amount;
+        if(expense.user_id = this.user_id){
+          this.myTotal += expense.amount
+        }
+      }
+      
       console.log(data);
     });
 
   }
 
-  form = this.fb.group({
-    "title": ['',],
-    "amount": [0,]
-  });
-
   addGroupExpense() {
     let expense: expenseToAdd = {
       title: this.form.getRawValue().title,
       amount: this.form.getRawValue().amount,
-      user_id: 1,
-      group_id: 1,
+      user_id: this.user_id,
+      group_id: this.group_id,
     }
 
-    this.apiService.addGroupExpense(expense)
+    this.apiService.addGroupExpense(expense).catch((error:any)=>{});
   }
 
 }
